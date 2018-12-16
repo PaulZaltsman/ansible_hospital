352195eded0c88bd1f36a1c27b7c9f76e4124e9d
PaulZaltsman <paulz@sela.co.il>
2018-12-16 19:30:52 +0200

    some fixes

--- deploy.sh
+++ deploy.sh
@@ -1,35 +1,6 @@
 #!/bin/bash
-
-setup_install_epel()
-{
-  yum install -y epel-release
-}
-
-setup_install_base_utils()
-{
-  yum install -y unzip sshpass ansible
-}
-
-getTimestamp() {
-  echo $(date -d now +%s)
-}
-
-printTimeStats()
-{
-    echo
-    echo "----Time statistics for $HOSPITAL ----"
-    echo
-    echo "Started at : $(date -d @$DEPLOYMENT_START +%T)"
-    echo
-    echo "Ended at   : $(date -d @$DEPLOYMENT_END +%T)"
-    echo
-    echo -n "Elpased time : " ; date -d@$(($DEPLOYMENT_END - $DEPLOYMENT_START)) -u +%H:%M:%S ; echo
-    echo
-    echo "-----------------------"
-    echo
-}
-
-usage="Usage: $(basename "$0") -h [hospital-name]"
+source ./deploy_functions.sh
+usage="Usage: $(basename "$0")-h [hospital-name]"
 
 while getopts "h:" opt; do
   case $opt in
---
+++ deploy_functions.sh
@@ -0,0 +1,36 @@
+
+setup_install_epel()
+{
+  yum install -y epel-release
+}
+
+setup_install_base_utils()
+{
+  yum install -y unzip sshpass ansible
+}
+
+getTimestamp() {
+  echo $(date -d now +%s)
+}
+
+printTimeStats()
+{
+    echo
+    echo "----Time statistics for $HOSPITAL ----"
+    echo
+    echo "Started at : $(date -d @$DEPLOYMENT_START +%T)"
+    echo
+    echo "Ended at   : $(date -d @$DEPLOYMENT_END +%T)"
+    echo
+    echo -n "Elpased time : " ; date -d@$(($DEPLOYMENT_END - $DEPLOYMENT_START)) -u +%H:%M:%S ; echo
+    echo
+    echo "-----------------------"
+    echo
+}
+
+
+build_configuration()
+{
+  ansible-playbook conf.yaml -i localhost -e "$HOSPITAL=hospital || exit $?"
+
+}
